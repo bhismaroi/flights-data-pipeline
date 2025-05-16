d2d155f58ca621461429312fa76a01659c48cabe
root <root@NWJKT50.MOLJKT.com>
2025-05-16 15:20:11 +0700

    Add Flights Data Pipeline project with large files via Git LFS

--- include/transformations/dim_aircrafts.sql
+++ include/transformations/dim_aircrafts.sql
@@ -1,30 +1,3 @@
-INSERT INTO final.dim_aircraft (
-    aircraft_id,
-    aircraft_nk,
-    model,
-    range
-)
-
-SELECT
-    ad.id AS aircraft_id,
-    ad.aircraft_code AS aircraft_nk,
-    ad.model,
-    ad.range
-    
-FROM
-    stg.aircrafts_data ad 
-    
-ON CONFLICT(aircraft_id) 
-DO UPDATE SET
-    aircraft_nk = EXCLUDED.aircraft_nk,
-    model = EXCLUDED.model,
-    range = EXCLUDED.range,
-    updated_at = CASE WHEN 
-                        final.dim_aircraft.aircraft_nk <> EXCLUDED.aircraft_nk
-                        OR final.dim_aircraft.model <> EXCLUDED.model
-                        OR final.dim_aircraft.range <> EXCLUDED.range
-                THEN 
-                        CURRENT_TIMESTAMP
-                ELSE
-                        final.dim_aircraft.updated_at
-                END;
+version https://git-lfs.github.com/spec/v1
+oid sha256:b35ff49a038671a1272e4db1e34e1ee3f334e0cf663df55fb792f4b69d6845fd
+size 772
